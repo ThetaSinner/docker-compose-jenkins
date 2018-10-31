20cf17bf7c60619999ff7f7e51d97d22f0832b3d
ThetaSinner <greatdjonfire@hotmail.co.uk>
2018-10-31 20:06:17 +0000

    use hostnames for the compose containers

--- jenkins-slave-dotnet/startup.sh
+++ jenkins-slave-dotnet/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget http://172.21.0.2:8080/jnlpJars/agent.jar
+wget http://jenkins-master:8080/jnlpJars/agent.jar
 
-/opt/java/jdk-11.0.1/bin/java -jar /var/jenkins/agent.jar -jnlpUrl http://172.21.0.2:8080/computer/dotnet-slave/slave-agent.jnlp -secret 9f783519dbdc5eb91f69447d7714f8d08a0a0ad8f761f342d43672225cc943ec -workDir "/var/jenkins"
+/opt/java/jdk-11.0.1/bin/java -jar /var/jenkins/agent.jar -jnlpUrl http://jenkins-master:8080/computer/dotnet-slave/slave-agent.jnlp -secret 9f783519dbdc5eb91f69447d7714f8d08a0a0ad8f761f342d43672225cc943ec -workDir "/var/jenkins"
